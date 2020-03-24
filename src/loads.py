3393d594c437b9dccf692aa469d1ec45c102b9f8
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2020-03-24 16:59:24 -0700

    Update loads processor to add single load objects for each zone list (building area type), see json file for reference.

--- src/loads.py
+++ src/loads.py
@@ -10,32 +10,29 @@ IDF.setiddname("../resources/Energy+V9_0_1.idd")
 class Loads:
     def __init__(self, case: Dict, idf: IDF):
         self.idf = idf
-        self.electric_equipment = case["internal_loads"]["electric_equipment"]
-        self.lighting = case["internal_loads"]["lighting"]
-        self.infiltration = case["internal_loads"]["infiltration"]
-        self.people = case["internal_loads"]["people"]
-        self.zones = []
-        # Retrieves a triple with the zone id, zone name and zone name in idf
-        for i, zn in case["zone_geometry"].items():
-            for zn_idf in idf.idfobjects["ZONE"]:
-                zn_idf_name = zn_idf.Name
-                if zn["name"] in zn_idf_name:
-                    self.zones.append((i, zn["name"], zn_idf_name))
+        self.building_area_type = case["building_area_type"]
+        b_a_t = self.building_area_type
+        self.electric_equipment = case["internal_loads"][b_a_t]["electric_equipment"]
+        self.lighting = case["internal_loads"][b_a_t]["lighting"]
+        self.infiltration = case["internal_loads"][b_a_t]["infiltration"]
+        self.people = case["internal_loads"][b_a_t]["people"]
 
     def set_electric_equipment(self) -> IDF:
         """
         Set electric equipment loads
         """
         local_idf = IDF(StringIO(""))
-        for zn, zn_name, zn_idf_name in self.zones:
-            eqp = local_idf.newidfobject("ELECTRICEQUIPMENT")
-            eqp.Name = "{}_eqp".format(zn_name)
-            eqp.Zone_or_ZoneList_Name = zn_idf_name
-            eqp.Design_Level_Calculation_Method = "Watts/Area"
-            eqp.Watts_per_Zone_Floor_Area = self.electric_equipment[zn]["epd"]
-            eqp.Fraction_Latent = self.electric_equipment[zn]["frac_latent"]
-            eqp.Fraction_Radiant = self.electric_equipment[zn]["frac_radiant"]
-            eqp.Fraction_Lost = self.electric_equipment[zn]["frac_lost"]
+        eqp = local_idf.newidfobject("ELECTRICEQUIPMENT")
+        eqp.Name = "{}_eqp".format(self.building_area_type)
+        eqp.Zone_or_ZoneList_Name = "zone_list_{}".format(
+            self.building_area_type.strip()
+        )
+        eqp.Schedule_Name = self.electric_equipment["schedule"]
+        eqp.Design_Level_Calculation_Method = "Watts/Area"
+        eqp.Watts_per_Zone_Floor_Area = self.electric_equipment["epd"]
+        eqp.Fraction_Latent = self.electric_equipment["frac_latent"]
+        eqp.Fraction_Radiant = self.electric_equipment["frac_radiant"]
+        eqp.Fraction_Lost = self.electric_equipment["frac_lost"]
         return local_idf
 
     def set_lighting(self) -> IDF:
@@ -43,14 +40,16 @@ class Loads:
         Set lighting loads
         """
         local_idf = IDF(StringIO(""))
-        for zn, zn_name, zn_idf_name in self.zones:
-            lgt = local_idf.newidfobject("LIGHTS")
-            lgt.Name = "{}_lgt".format(zn_name)
-            lgt.Zone_or_ZoneList_Name = zn_idf_name
-            lgt.Design_Level_Calculation_Method = "Watts/Area"
-            lgt.Watts_per_Zone_Floor_Area = self.lighting[zn]["lpd"]
-            lgt.Fraction_Radiant = self.lighting[zn]["frac_radiant"]
-            lgt.Fraction_Visible = self.lighting[zn]["frac_visible"]
+        lgt = local_idf.newidfobject("LIGHTS")
+        lgt.Name = "{}_lgt".format(self.building_area_type)
+        lgt.Zone_or_ZoneList_Name = "zone_list_{}".format(
+            self.building_area_type.strip()
+        )
+        lgt.Schedule_Name = self.lighting["schedule"]
+        lgt.Design_Level_Calculation_Method = "Watts/Area"
+        lgt.Watts_per_Zone_Floor_Area = self.lighting["lpd"]
+        lgt.Fraction_Radiant = self.lighting["frac_radiant"]
+        lgt.Fraction_Visible = self.lighting["frac_visible"]
         return local_idf
 
     def set_infiltration(self) -> IDF:
@@ -58,17 +57,19 @@ class Loads:
         Set infiltration loads
         """
         local_idf = IDF(StringIO(""))
-        for zn, zn_name, zn_idf_name in self.zones:
-            inf = local_idf.newidfobject("ZONEINFILTRATION:DESIGNFLOWRATE")
-            inf.Name = "{}_inf".format(zn_name)
-            inf.Zone_or_ZoneList_Name = zn_idf_name
-            inf.Design_Flow_Rate_Calculation_Method = "Flow/ExteriorArea"
-            inf.Flow_per_Exterior_Surface_Area = self.infiltration[zn]["inf"]
-            # DOE-2 coefficients
-            inf.Constant_Term_Coefficient = 0
-            inf.Temperature_Term_Coefficient = 0
-            inf.Velocity_Term_Coefficient = 0.224
-            inf.Velocity_Squared_Term_Coefficient = 0
+        inf = local_idf.newidfobject("ZONEINFILTRATION:DESIGNFLOWRATE")
+        inf.Name = "{}_inf".format(self.building_area_type)
+        inf.Zone_or_ZoneList_Name = "zone_list_{}".format(
+            self.building_area_type.strip()
+        )
+        inf.Schedule_Name = self.infiltration["schedule"]
+        inf.Design_Flow_Rate_Calculation_Method = "Flow/ExteriorArea"
+        inf.Flow_per_Exterior_Surface_Area = self.infiltration["inf"]
+        # DOE-2 coefficients
+        inf.Constant_Term_Coefficient = 0
+        inf.Temperature_Term_Coefficient = 0
+        inf.Velocity_Term_Coefficient = 0.224
+        inf.Velocity_Squared_Term_Coefficient = 0
         return local_idf
 
     def set_people(self) -> IDF:
@@ -76,16 +77,21 @@ class Loads:
         Set occupants
         """
         local_idf = IDF(StringIO(""))
-        for zn, zn_name, zn_idf_name in self.zones:
-            ppl = local_idf.newidfobject("PEOPLE")
-            ppl.Name = "{}_ppl".format(zn_name)
-            ppl.Zone_or_ZoneList_Name = zn_idf_name
-            ppl.Number_of_People_Calculation_Method = "People/Area"
-            ppl.People_per_Zone_Floor_Area = self.people[zn]["people"]
-            ppl.Fraction_Radiant = self.people[zn]["frac_radiant"]
+        ppl = local_idf.newidfobject("PEOPLE")
+        ppl.Name = "{}_ppl".format(self.building_area_type)
+        ppl.Zone_or_ZoneList_Name = "zone_list_{}".format(
+            self.building_area_type.strip()
+        )
+        ppl.Number_of_People_Schedule_Name = self.people["people_schedule"]
+        ppl.Number_of_People_Calculation_Method = "People/Area"
+        ppl.People_per_Zone_Floor_Area = self.people["people"]
+        ppl.Fraction_Radiant = self.people["frac_radiant"]
+        ppl.Activity_Level_Schedule_Name = self.people["people_activity_schedule"]
         return local_idf
 
-    def set_loads(self, equipment=True, lighting=True, infiltration=True, people=True) -> IDF:
+    def set_loads(
+        self, equipment=True, lighting=True, infiltration=True, people=True
+    ) -> IDF:
         """
         Set all requested loads
         """
