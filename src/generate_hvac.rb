e1ef9b98f801cf2ae0ab2d854f569ad885566df8
Xuechen (Jerry) Lei <xuechen.lei@pnnl.gov>
2020-06-03 12:22:59 -0700

    take out shw loop from openstudio call

--- src/generate_hvac.rb
+++ src/generate_hvac.rb
@@ -1,10 +1,10 @@
-require 'openstudio'
-require 'openstudio-standards'
-standard = Standard.build('90.1-2013')
+require "openstudio"
+require "openstudio-standards"
+standard = Standard.build("90.1-2013")
 idf = OpenStudio::Workspace.load("../hvac_dev/zones.idf").get
-trans = OpenStudio::EnergyPlus::ReverseTranslator.new 
+trans = OpenStudio::EnergyPlus::ReverseTranslator.new
 osm = trans.translateWorkspace(idf)
-standard.model_add_hvac_system(osm, 'PSZ-AC', 'NaturalGas', nil, nil, osm.getThermalZones)
+standard.model_add_hvac_system(osm, "PSZ-AC", "NaturalGas", nil, nil, osm.getThermalZones, air_loop_heating_type: "Gas")
 trans = OpenStudio::EnergyPlus::ForwardTranslator.new
 idf = trans.translateModel(osm)
-idf.save('../hvac_dev/zones_hvacadded.idf', true)
+idf.save("../hvac_dev/zones_hvacadded.idf", true)
