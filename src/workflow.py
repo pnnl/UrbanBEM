e3f9478e776895b12d91a057c03033865d9dcb89
Xuechen (Jerry) Lei <xuechen.lei@pnnl.gov>
2020-07-21 14:37:07 -0700

    add weather based ground temperature files.

--- src/workflow.py
+++ src/workflow.py
@@ -51,7 +51,7 @@ geometryadded_obj = Geometry(proc_case, idf)
 geometryadded_obj.save_idf("../devoutput/geometry_added.idf")
 
 #%% construction processor
-constructionadded_obj = Constructions(proc_case, idf)
+constructionadded_obj = Constructions(proc_case, geometryadded_obj.idf)
 constructionadded_obj.save_idf("../devoutput/construction_added.idf")
 
 # %% schedule processor
