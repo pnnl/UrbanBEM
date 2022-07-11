bc28f39746d31c23bccc0b187912d115544dfc29
Xiaoqi Liu <xiaoqi.liu@pnnl.gov>
2022-07-11 14:42:30 -0500

    find fenestrationsurface:detailed object names instead of hardcode window names

--- src/overhang.py
+++ src/overhang.py
@@ -11,8 +11,6 @@ class Overhang:
     def __init__(self, case: Dict, idf: IDF):
         
         self.idf = idf
-        self.number_of_floor = int(case["zone_geometry"][0]["num_floors"])
-        self.building_area_type = case["building_area_type"]
         overhangflag = False
         
         if case["overhang"]["has_overhang"].lower() == "yes":
@@ -26,63 +24,23 @@ class Overhang:
         """Set overhang objects"""
         
         local_idf = IDF(StringIO(""))
+        windows = self.idf.idfobjects["FENESTRATIONSURFACE:DETAILED"]
         
-        for floor in range(self.number_of_floor):
-            
-            window_name_0001 = "Block " + self.building_area_type + "_east_zone Storey " + str(floor) + " Wall 0001 window"
-            window_name_0002 = "Block " + self.building_area_type + "_north_zone Storey " + str(floor) + " Wall 0002 window"
-            window_name_0003 = "Block " + self.building_area_type + "_west_zone Storey " + str(floor) + " Wall 0003 window"
-            window_name_0004 = "Block " + self.building_area_type + "_south_zone Storey " + str(floor) + " Wall 0004 window"
-            
-            overhang_0001_dict = {
+        for window in windows:
+
+            print(window.Name)
+            overhang_dict = {
                 "key": "SHADING:OVERHANG",
-                "Name": window_name_0001 + " overhang",
-                "Window_or_Door_Name": window_name_0001,
+                "Name": window.Name + " overhang",
+                "Window_or_Door_Name": window.Name,
                 "Height_above_Window_or_Door": 0.1,
                 "Tilt_Angle_from_WindowDoor": 90,
                 "Left_extension_from_WindowDoor_Width": 0,
                 "Right_extension_from_WindowDoor_Width": 0,
                 "Depth": 0.5,
             }
-            
-            overhang_0002_dict = {
-                "key": "SHADING:OVERHANG",
-                "Name": window_name_0002 + " overhang",
-                "Window_or_Door_Name": window_name_0002,
-                "Height_above_Window_or_Door": 0.1,
-                "Tilt_Angle_from_WindowDoor": 90,
-                "Left_extension_from_WindowDoor_Width": 0,
-                "Right_extension_from_WindowDoor_Width": 0,
-                "Depth": 0.5,
-            }
-            
-            overhang_0003_dict = {
-                "key": "SHADING:OVERHANG",
-                "Name": window_name_0003 + " overhang",
-                "Window_or_Door_Name": window_name_0003,
-                "Height_above_Window_or_Door": 0.1,
-                "Tilt_Angle_from_WindowDoor": 90,
-                "Left_extension_from_WindowDoor_Width": 0,
-                "Right_extension_from_WindowDoor_Width": 0,
-                "Depth": 0.5,
-            }
-            
-            overhang_0004_dict = {
-                "key": "SHADING:OVERHANG",
-                "Name": window_name_0004 + " overhang",
-                "Window_or_Door_Name": window_name_0004,
-                "Height_above_Window_or_Door": 0.1,
-                "Tilt_Angle_from_WindowDoor": 90,
-                "Left_extension_from_WindowDoor_Width": 0,
-                "Right_extension_from_WindowDoor_Width": 0,
-                "Depth": 0.5,
-            }
-        
-        
-            local_idf.newidfobject(**overhang_0001_dict)
-            local_idf.newidfobject(**overhang_0002_dict)
-            local_idf.newidfobject(**overhang_0003_dict)
-            local_idf.newidfobject(**overhang_0004_dict)
+
+            local_idf.newidfobject(**overhang_dict)
 
         return local_idf
 
